99573f9795ce514e4a29e7cbcb9e27a20a68e882
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-17 12:55:38 -0600

    changed image to img, because it was causing an error

--- models.py
+++ models.py
@@ -16,9 +16,9 @@ class Cupcake(db.Model):
     flavor = db.Column(db.Text, nullable=False)
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
-    image = db.Column(db.Text, nullable=False,
+    img = db.Column(db.Text, nullable=False,
                       default="https://tinyurl.com/demo-cupcake")
 
     def serialize(self):
         return {"id": self.id, "flavor": self.flavor, "size": self.size,
-                "rating": self.rating, "image": self.image}
+                "rating": self.rating, "img": self.img}
