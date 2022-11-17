09add6ef562ab50d78bf88de903be94e5c2f2b39
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-17 12:24:35 -0600

    did not know how to seed from seed.py so i changed it to seed.sql and changed the logic to match

---
+++ seed.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS cupcake;
+
+CREATE DATABASE cupcake;
+
+\c cupcake
+CREATE TABLE cupcakes (
+    id serial PRIMARY KEY,
+    flavor text NOT NULL,
+    size text NOT NULL,
+    rating float NOT NULL,
+    image text NOT NULL DEFAULT 'https://tinyurl.com/demo-cupcake'
+);
+
+INSERT INTO cupcakes (flavor, size, rating, image)
+    VALUES ('cherry', 'large', 5, DEFAULT), ('chocolate', 'small', 9, 'https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg');
+
