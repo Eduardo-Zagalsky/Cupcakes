897ddc859dcb68597c237c125fda7c04e2c4d717
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-17 01:37:30 -0600

    added 3 routes for the json restfulAPI

--- app.py
+++ app.py
@@ -15,3 +15,32 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 
 
+@app.route("/api/cupcakes")
+def get_cupcakes():
+    serialized = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=serialized)
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route("/api/cupcakes", methods=["POST"])
+def post_cupcakes():
+    cupcake = Cupcake.query.all()
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = cupcake.serialize(new_cupcake)
+
+    return jsonify(cupcake=serialized)
