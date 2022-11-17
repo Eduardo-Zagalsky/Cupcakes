3639715985c75ca75b89404e5c9988e59f197f6f
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-17 12:54:46 -0600

    changed image to img, because it was causing an error

--- seed.sql
+++ seed.sql
@@ -8,9 +8,9 @@ CREATE TABLE cupcakes (
     flavor text NOT NULL,
     size text NOT NULL,
     rating float NOT NULL,
-    image text NOT NULL DEFAULT 'https://tinyurl.com/demo-cupcake'
+    img text NOT NULL DEFAULT 'https://tinyurl.com/demo-cupcake'
 );
 
-INSERT INTO cupcakes (flavor, size, rating, image)
+INSERT INTO cupcakes (flavor, size, rating, img)
     VALUES ('cherry', 'large', 5, DEFAULT), ('chocolate', 'small', 9, 'https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg');
 
