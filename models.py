c60e15203683c3b9b13afdc29af6c965300f2416
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2022-11-17 01:21:03 -0600

    made it look nicer

--- models.py
+++ models.py
@@ -16,10 +16,9 @@ class Cupcake(db.Model):
     flavor = db.Column(db.Text, nullable=False)
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
-    image = db.Column(db.Text, nullable=False,default="https://tinyurl.com/demo-cupcake")
+    image = db.Column(db.Text, nullable=False,
+                      default="https://tinyurl.com/demo-cupcake")
 
     def serialize(self):
-        return{
-            {"id": self.id, "flavor": self.flavor, "size": self.size,
-                          "rating": self.rating, "image": self.image}
-        }
+        return {"id": self.id, "flavor": self.flavor, "size": self.size,
+                "rating": self.rating, "image": self.image}
